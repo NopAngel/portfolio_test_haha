7230e0c7af65267e0f46fdb09a2d768cba687a06
NoAngel <155915830+NopAngel@users.noreply.github.com>
2025-02-02 13:14:31 -0400

    Update main.js

--- template/main.js
+++ template/main.js
@@ -1,12 +1,1 @@
-  document.addEventListener("DOMContentLoaded", function() {
-      var element = document.getElementById('animatedElement');
-      function checkPosition() {
-        var windowHeight = window.innerHeight;
-        var positionFromTop = element.getBoundingClientRect().top;
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add('visible');
-        }
-      }
-      window.addEventListener('scroll', checkPosition);
-      checkPosition();
-    });
+
